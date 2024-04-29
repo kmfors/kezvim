604e43c0739ed52a64688f9d0dc79b312c9af3b6
kmfors <kmfors@aliyun.com>
2024-04-29 18:14:31 +0800

    修复浮动终端无法滚轮移动问题

--- lua/nvim/lua/plugins/toggleterm.lua
+++ lua/nvim/lua/plugins/toggleterm.lua
@@ -1,6 +1,6 @@
 return {
 
-------------------------------- 终端窗口 ------------------------------
+    ------------------------------- 终端窗口 ------------------------------
     'akinsho/toggleterm.nvim', version = "*", 
     event = "VeryLazy",
 
@@ -18,67 +18,67 @@ return {
             dir = "git_dir",
         })
 
-        --function _G.set_terminal_keymaps()
-            --  local opts = {buffer = 0}
-            --  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-            --  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-            --  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-            --  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-            --  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-            --  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-            --  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-            --end
-
-            ---- if you only want these mappings for toggle term use term://*toggleterm#* instead
-            --vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+        function _G.set_terminal_keymaps()
+            local opts = {buffer = 0}
+            vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+            vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+            vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+            vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+            vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+            vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+            vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
         end
+
+        ---- if you only want these mappings for toggle term use term://*toggleterm#* instead
+        vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+    end
 
 }
 --'akinsho/toggleterm.nvim', version = "*", config = true,
 --event = "VeryLazy",
 --
 --config = function()
---  require('toggleterm').setup({
---    size = 0.5,
---    open_mapping = [[<c-\>]],
---    hide_numbers = true,
---    shade_filetypes = true,           -- 在当前buffer的下方打开新终端
---    shade_terminals = true,
---    shading_factor = 2,
---    start_in_insert = true,         -- 打开新终端后自动进入插入模式
---    insert_mappings = true,
---    persist_size = true,            -- 使用默认尺寸
---    --direction = 'horizontal',
---    direction = 'float',
---    close_on_exit = true,
---    shell = vim.o.shell,
---    highlights = {
---      border = "Normal",
---      background = "Normal",
---    },
---    float_opt = {
---    size = 0.5,
---      width = 30,
---      height = 5300,
---      border = "curved",
---      winblend = 3,
---    },
---  })
---
---  --function _G.set_terminal_keymaps()
---  --  local opts = {buffer = 0}
---  --  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
---  --  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
---  --  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
---  --  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
---  --  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
---  --  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
---  --  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
---  --end
---
---  ---- if you only want these mappings for toggle term use term://*toggleterm#* instead
---  --vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
---end
---
+    --  require('toggleterm').setup({
+        --    size = 0.5,
+        --    open_mapping = [[<c-\>]],
+        --    hide_numbers = true,
+        --    shade_filetypes = true,           -- 在当前buffer的下方打开新终端
+        --    shade_terminals = true,
+        --    shading_factor = 2,
+        --    start_in_insert = true,         -- 打开新终端后自动进入插入模式
+        --    insert_mappings = true,
+        --    persist_size = true,            -- 使用默认尺寸
+        --    --direction = 'horizontal',
+        --    direction = 'float',
+        --    close_on_exit = true,
+        --    shell = vim.o.shell,
+        --    highlights = {
+            --      border = "Normal",
+            --      background = "Normal",
+            --    },
+            --    float_opt = {
+                --    size = 0.5,
+                --      width = 30,
+                --      height = 5300,
+                --      border = "curved",
+                --      winblend = 3,
+                --    },
+                --  })
+                --
+                --  --function _G.set_terminal_keymaps()
+                    --  --  local opts = {buffer = 0}
+                    --  --  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+                    --  --  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+                    --  --  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+                    --  --  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+                    --  --  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+                    --  --  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+                    --  --  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+                    --  --end
+                    --
+                    --  ---- if you only want these mappings for toggle term use term://*toggleterm#* instead
+                    --  --vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+                    --end
+                    --
 
 
